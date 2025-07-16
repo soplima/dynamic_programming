c5afeae98ac40ad35f6215d7af015e4e5cbf84e3
soplima <sop.lima13@gmail.com>
2025-07-16 14:13:48 -0300

    Initial commit

---
+++ climbStairs.py
@@ -0,0 +1,22 @@
+# Example 1:
+
+# Input: n = 2
+# Output: 2
+# Explanation: There are two ways to climb to the top.
+# 1. 1 step + 1 step
+# 2. 2 steps
+
+# Example 2:
+# Input: n = 3
+# Output: 3
+# Explanation: There are three ways to climb to the top.
+# 1. 1 step + 1 step + 1 step
+# 2. 1 step + 2 steps
+# 3. 2 steps + 1 step
+
+class Solution(object):
+    def climbStairs(self, n):
+        if n == 0 or n == 1:
+            return 1
+        return self.climbStairs(n - 1) + self.climbStairs(n - 2)
+        
