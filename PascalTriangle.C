f5f56e5d705e929624bda46e8c5ed683e52bef03
soplima <sop.lima13@gmail.com>
2025-07-17 18:27:21 -0300

    feat: returns Pascal Triangle

---
+++ PascalTriangle.C
@@ -0,0 +1,28 @@
+// 118. Pascal's Triangle
+// Easy
+// Topics
+// premium lock icon
+// Companies
+// Given an integer numRows, return the first numRows of Pascal's triangle.
+
+// In Pascal's triangle, each number is the sum of the two numbers directly above it as shown:
+
+// Example 1:
+
+// Input: numRows = 5
+// Output: [[1],[1,1],[1,2,1],[1,3,3,1],[1,4,6,4,1]]
+
+/**
+ * Return an array of arrays of size *returnSize.
+ * The sizes of the arrays are returned as *returnColumnSizes array.
+ * Note: Both returned array and *columnSizes array must be malloced, assume caller calls free().
+ */
+int** generate(int numRows, int* returnSize, int** returnColumnSizes) {
+    int size = 0;
+    for(int i = 0; i < numRows; i++){
+        size++;
+    }
+    int malloc sizeof * (size);
+    
+
+}
